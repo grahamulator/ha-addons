ad4a7c1755f33521130218dc3a8e1d4e902c3582
Graham Henke <graham@twingate.com>
2025-02-07 11:46:58 -0500

    Run curl as sudo

--- tg_connector/run.sh
+++ tg_connector/run.sh
@@ -10,6 +10,6 @@ echo "Network: ${NETWORK}"
 echo "Access token: ${ACCESS_TOKEN}"
 echo "Refresh token: ${REFRESH_TOKEN}"
 
-curl "https://binaries.twingate.com/connector/setup.sh" | sudo TWINGATE_ACCESS_TOKEN="${ACCESS_TOKEN}" TWINGATE_REFRESH_TOKEN="${REFRESH_TOKEN}" TWINGATE_URL="https://${NETWORK}" TWINGATE_LABEL_DEPLOYED_BY="linux" bash
+sudo curl "https://binaries.twingate.com/connector/setup.sh" | sudo TWINGATE_ACCESS_TOKEN="${ACCESS_TOKEN}" TWINGATE_REFRESH_TOKEN="${REFRESH_TOKEN}" TWINGATE_URL="https://${NETWORK}" TWINGATE_LABEL_DEPLOYED_BY="linux" bash
 
 echo "Twingate Connector Deployed."
