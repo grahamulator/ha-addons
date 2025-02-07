356e35aaa2af0451a29f1d7a8661675ce65de8d0
Graham Henke <graham@twingate.com>
2025-02-07 11:53:17 -0500

    Use docker command

--- tg_connector/run.sh
+++ tg_connector/run.sh
@@ -10,6 +10,6 @@ echo "Network: ${NETWORK}"
 echo "Access token: ${ACCESS_TOKEN}"
 echo "Refresh token: ${REFRESH_TOKEN}"
 
-sudo curl "https://binaries.twingate.com/connector/setup.sh" | sudo TWINGATE_ACCESS_TOKEN="${ACCESS_TOKEN}" TWINGATE_REFRESH_TOKEN="${REFRESH_TOKEN}" TWINGATE_URL="https://${NETWORK}" TWINGATE_LABEL_DEPLOYED_BY="linux" bash
+docker run -d --sysctl net.ipv4.ping_group_range="0 2147483647" --env TWINGATE_URL="https://${NETWORK}" --env TWINGATE_ACCESS_TOKEN="${ACCESS_TOKEN}" --env TWINGATE_REFRESH_TOKEN="${REFRESH_TOKEN}"  --env TWINGATE_LABEL_HOSTNAME="`hostname`" --env TWINGATE_LABEL_DEPLOYED_BY="docker" --name "twingate-radical-boa" --restart=unless-stopped --pull=always twingate/connector:1
 
 echo "Twingate Connector Deployed."
