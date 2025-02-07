f35750b9edd97ca4fa6ae7617e258fda810ea55b
Graham Henke <graham@twingate.com>
2025-02-07 11:40:51 -0500

    Update run.sh with actual connector command

--- tg_connector/run.sh
+++ tg_connector/run.sh
@@ -6,6 +6,10 @@ NETWORK=$(bashio::config 'network_slug')
 ACCESS_TOKEN=$(bashio::config 'access_token')
 REFRESH_TOKEN=$(bashio::config 'refresh_token')
 
-echo "Network: ${NETWORK}."
-echo "Access token: ${ACCESS_TOKEN}."
-echo "Refresh token: ${REFRESH_TOKEN}."
+echo "Network: ${NETWORK}"
+echo "Access token: ${ACCESS_TOKEN}"
+echo "Refresh token: ${REFRESH_TOKEN}"
+
+curl "https://binaries.twingate.com/connector/setup.sh" | sudo TWINGATE_ACCESS_TOKEN="${ACCESS_TOKEN}" TWINGATE_REFRESH_TOKEN="${REFRESH_TOKEN}" TWINGATE_URL="https://${NETWORK}" TWINGATE_LABEL_DEPLOYED_BY="linux" bash
+
+echo "Twingate Connector Deployed."
