6a2a6174059a8016c0b077704dcd9acef29b8f2b
Graham Henke <graham@twingate.com>
2025-02-08 11:03:50 -0500

    Add connector_name arg

--- tg_connector/run.sh
+++ tg_connector/run.sh
@@ -3,6 +3,7 @@
 echo "Starting Twingate Connector..."
 
 NETWORK=$(bashio::config 'network_slug')
+NAME=$(bashio::config 'connector_name')
 ACCESS_TOKEN=$(bashio::config 'access_token')
 REFRESH_TOKEN=$(bashio::config 'refresh_token')
 
@@ -10,6 +11,6 @@ echo "Network: ${NETWORK}"
 echo "Access token: ${ACCESS_TOKEN}"
 echo "Refresh token: ${REFRESH_TOKEN}"
 
-docker run -d --sysctl net.ipv4.ping_group_range="0 2147483647" --env TWINGATE_URL="https://${NETWORK}" --env TWINGATE_ACCESS_TOKEN="${ACCESS_TOKEN}" --env TWINGATE_REFRESH_TOKEN="${REFRESH_TOKEN}"  --env TWINGATE_LABEL_HOSTNAME="`hostname`" --env TWINGATE_LABEL_DEPLOYED_BY="docker" --name "twingate-radical-boa" --restart=unless-stopped --pull=always twingate/connector:1
+docker run -d --sysctl net.ipv4.ping_group_range="0 2147483647" --env TWINGATE_URL="https://${NETWORK}" --env TWINGATE_ACCESS_TOKEN="${ACCESS_TOKEN}" --env TWINGATE_REFRESH_TOKEN="${REFRESH_TOKEN}"  --env TWINGATE_LABEL_HOSTNAME="`hostname`" --env TWINGATE_LABEL_DEPLOYED_BY="docker" --name "${NAME}" --restart=unless-stopped --pull=always twingate/connector:1
 
 echo "Twingate Connector Deployed."
